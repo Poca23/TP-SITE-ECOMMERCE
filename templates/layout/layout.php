288dd5227b4f45c05bc5719559d8694134207e26
Poca23 <cndweb37@gmail.com>
2026-02-27 16:31:07 +0100

    update title

--- templates/layout/layout.php
+++ templates/layout/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UnicornShop 🦄</title>
+    <title>UnicornShop</title>
     <link rel="icon" href="https://fav.farm/🦄" type="image/png">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
